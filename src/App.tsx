ae6586062b3673136977a665133d91bdf4e86927
Angus Bezzina <37071175+angusbezzina@users.noreply.github.com>
2025-11-19 12:39:41 -0500

    chore: hide unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import './App.css'
 
-import { About } from './components/About'
-import { Cta } from './components/Cta'
+// import { About } from './components/About'
+// import { Cta } from './components/Cta'
 import { FAQ } from './components/FAQ'
-import { Features } from './components/Features'
+// import { Features } from './components/Features'
 import { Footer } from './components/Footer'
 import { Hero } from './components/Hero'
-import { HowItWorks } from './components/HowItWorks'
+// import { HowItWorks } from './components/HowItWorks'
 import { Integrations } from './components/Integrations'
 import { Navbar } from './components/Navbar'
-import { Newsletter } from './components/Newsletter'
-import { Pricing } from './components/Pricing'
+// import { Newsletter } from './components/Newsletter'
+// import { Pricing } from './components/Pricing'
 import { ScrollToTop } from './components/ScrollToTop'
-import { Services } from './components/Services'
+// import { Services } from './components/Services'
 import { Signup } from './components/Signup'
-import { Team } from './components/Team'
-import { Testimonials } from './components/Testimonials'
+// import { Team } from './components/Team'
+// import { Testimonials } from './components/Testimonials'
 import { Workflow } from './components/Workflow'
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
       <Signup />
       <FAQ />
       {/* TODO: From here down, don't show for now? */}
-      <About />
+      {/* <About />
       <HowItWorks />
       <Features />
       <Services />
@@ -36,7 +36,7 @@ function App() {
       <Testimonials />
       <Team />
       <Pricing />
-      <Newsletter />
+      <Newsletter /> */}
       <Footer />
       <ScrollToTop />
     </>
